b35720c61c59aeb959b81c6b357623507e495502
SamPom100 <sampom100@gmail.com>
2020-06-01 22:39:27 -0500

    added final touches

--- main.py
+++ main.py
@@ -10,48 +10,40 @@ reddit = praw.Reddit(client_id=secret.client_id,
 
 start = time.time()
 
-
-# assume you have a Reddit instance bound to variable `reddit`
 print(spacer)
 subreddit = reddit.subreddit("wallstreetbets")
 
 print("Accessing: " + subreddit.display_name)
-# print(subreddit.description)
 
 print(spacer)
-
 
 print("Getting hot comments from " + subreddit.display_name)
 print(spacer)
 masterSET = set()
 
 for submission in subreddit.hot(limit=10):
-    # Output: the submission's title
     masterSET.add(submission.title.lower())
-    submission.comments.replace_more(limit=10)
+    submission.comments.replace_more(limit=None)
     for comment in submission.comments.list():
         masterSET.add(comment.body.lower())
 
 print("Done")
-
 print(spacer)
 
 autistCount = sum('autist' in s for s in masterSET)
 callCount = sum('call' in s for s in masterSET)
 putCount = sum('put' in s for s in masterSET)
+
 print("Autist Count: " + str(autistCount))
 print("Call Count: " + str(callCount))
 print("Put Count: " + str(putCount))
 
 print(spacer)
 if(callCount > putCount):
-    print("BULLISH by " + str(100*(callCount / (callCount+putCount)))+"%")
+    print("BULLISH by " + str(round(100*(callCount/(callCount + putCount)), 3)))
 else:
-    print("BEARISH by " + str(100*(putCount / (callCount+putCount)))+"%")
-# print(submission.score)  # Output: the submission's score
-# print(submission.id)     # Output: the submission's ID
-# print(submission.url)    # Output: the URL the submission points to
-# or the submission's URL if it's a self post
+    print("BEARISH by " + str(round(100*(putCount/(callCount + putCount)), 3)))
 
+print(spacer)
 end = time.time()
-print(end - start)
+print("Function Time: " + end - start)
