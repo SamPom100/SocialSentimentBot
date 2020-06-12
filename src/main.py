d2001adf4b25ed9a87999d3424b8fdedfdf13b93
SamPom100 <sampom100@gmail.com>
2020-06-12 17:52:01 -0500

    added new photo

--- src/main.py
+++ src/main.py
@@ -30,11 +30,11 @@ masterSET = []
 
 commentCounter = 0
 
-for submission in subreddit.hot(limit=20):  # .hot, .new
+for submission in subreddit.hot(limit=30):  # .hot, .new
     for sub in submission.title.upper().split(" "):
         masterSET.append(sub)
         commentCounter += 1
-    submission.comments.replace_more(limit=20)
+    submission.comments.replace_more(limit=40)
     for comment in submission.comments.list():
         for sub2 in comment.body.upper().split(" "):
             masterSET.append(sub2)
