84ae3270af7305f3118a25fc147ae6e53eb6ec61
SamPom100 <sampom100@gmail.com>
2020-07-06 15:12:53 -0500

    fixed bug with variables

--- src/main.py
+++ src/main.py
@@ -15,112 +15,107 @@ reddit = praw.Reddit(client_id=secret.client_id,
                      client_secret=secret.client_secret, user_agent=secret.user_agent)
 
 
-def getSentiment(postNum, commentNum):
+start = time.time()
 
-    start = time.time()
+print(spacer)
+subreddit = reddit.subreddit("wallstreetbets")
 
-    print(spacer)
-    subreddit = reddit.subreddit("wallstreetbets")
+print("Accessing: " + subreddit.display_name.upper())
 
-    print("Accessing: " + subreddit.display_name.upper())
+print(spacer)
 
-    print(spacer)
+print("Getting posts / comments from " +
+      subreddit.display_name.upper())
+print(spacer)
+masterSET = []
 
-    print("Getting posts / comments from " +
-          subreddit.display_name.upper())
-    print(spacer)
-    masterSET = []
+commentCounter = 0
 
-    commentCounter = 0
-
-    for submission in subreddit.hot(limit=postNum):  # .hot, .new
-        for sub in submission.title.upper().split(" "):
-            masterSET.append(sub)
+for submission in subreddit.hot(limit=20):  # .hot, .new
+    for sub in submission.title.upper().split(" "):
+        masterSET.append(sub)
+        commentCounter += 1
+    submission.comments.replace_more(limit=20)
+    for comment in submission.comments.list():
+        for sub2 in comment.body.upper().split(" "):
+            masterSET.append(sub2)
             commentCounter += 1
-        submission.comments.replace_more(limit=commentNum)
-        for comment in submission.comments.list():
-            for sub2 in comment.body.upper().split(" "):
-                masterSET.append(sub2)
-                commentCounter += 1
-                if commentCounter % 1000 == 0:
-                    print("\nComments Analyzed: "+str(commentCounter))
+            if commentCounter % 1000 == 0:
+                print("\nComments Analyzed: "+str(commentCounter))
 
-    print("Analyzing")
-    print(spacer)
+print("Analyzing")
+print(spacer)
 
-    autistCount = sum('AUTIST' in s for s in masterSET)
-    callCount = sum('CALL' in s for s in masterSET)
-    putCount = sum('PUT' in s for s in masterSET)
+autistCount = sum('AUTIST' in s for s in masterSET)
+callCount = sum('CALL' in s for s in masterSET)
+putCount = sum('PUT' in s for s in masterSET)
 
-    print("Autist Count: " + str(autistCount))
-    print("Call Count: " + str(callCount))
-    print("Put Count: " + str(putCount))
-    print("Total Comments Analyzed: "+str(commentCounter))
+print("Autist Count: " + str(autistCount))
+print("Call Count: " + str(callCount))
+print("Put Count: " + str(putCount))
+print("Total Comments Analyzed: "+str(commentCounter))
 
-    sentiment = "WSB is "
-    print(spacer)
-    if(callCount > putCount):
-        sentiment += "BULLISH by " + \
-            str(round(100*(callCount/(callCount + putCount)), 1)) + "%"
-    else:
-        sentiment += "BEARISH by " + \
-            str(round(100*(putCount/(callCount + putCount)), 1)) + "%"
+sentiment = "WSB is "
+print(spacer)
+if(callCount > putCount):
+    sentiment += "BULLISH by " + \
+        str(round(100*(callCount/(callCount + putCount)), 1)) + "%"
+else:
+    sentiment += "BEARISH by " + \
+        str(round(100*(putCount/(callCount + putCount)), 1)) + "%"
 
-    print(sentiment)
+print(sentiment)
 
-    slices = [callCount, putCount]
-    names = ['Calls', 'Puts']
-    colors = ['springgreen', 'lightcoral']
-    plt.figure(0)
-    plt.pie(slices, labels=names, colors=colors, autopct='%1.1f%%',
-            shadow=True, startangle=90, textprops={'fontsize': 17, 'weight': 'bold'})
-    #plt.title('Calls or Puts')
+slices = [callCount, putCount]
+names = ['Calls', 'Puts']
+colors = ['springgreen', 'lightcoral']
+plt.figure(0)
+plt.pie(slices, labels=names, colors=colors, autopct='%1.1f%%',
+        shadow=True, startangle=90, textprops={'fontsize': 17, 'weight': 'bold'})
+# plt.title('Calls or Puts')
 
-    print(spacer)
+print(spacer)
 
-    list_of_tickers = gt.get_tickers(NYSE=True, NASDAQ=True, AMEX=True)
+list_of_tickers = gt.get_tickers(NYSE=True, NASDAQ=True, AMEX=True)
 
-    tickerList = []
+tickerList = []
 
-    r_item = ['A', 'ON', 'IT', 'FOR', 'AT', 'ARE', 'BE', 'ALL', 'SO',
-              'GO', 'OR', 'CAN', 'HE', 'NOW', 'OUT', 'AN', 'HAS', 'BY', 'ONE',
-              'SEE', 'GOOD', 'BIG', 'ANY', 'NEW', 'AM', 'NEXT', 'WELL', 'PUMP', 'EVER',
-              'RUN', 'VERY', 'PLAY', 'DD', 'POST', 'ELSE', 'LOVE', 'TELL', 'BEST', 'LIFE',
-              'HOPE', 'TWO', 'NICE', 'BIT', 'MAN', 'TRUE', 'FUN', 'LOW', 'TECH', 'CAR', 'STAY',
-              'EOD', 'JOB', 'FLAT', 'OLD', 'RTX', 'HOME', 'OW', 'JOE', 'BEAT', 'WOW', 'X', 'ATH',
-              'SAVE', 'EAT', 'HUGE', 'PER', 'LIVE', 'CARE', 'PEAK', 'TURN', 'PLUS', 'HEAR', 'GAIN', 'BRO', 'RH']
+r_item = ['A', 'ON', 'IT', 'FOR', 'AT', 'ARE', 'BE', 'ALL', 'SO',
+          'GO', 'OR', 'CAN', 'HE', 'NOW', 'OUT', 'AN', 'HAS', 'BY', 'ONE',
+          'SEE', 'GOOD', 'BIG', 'ANY', 'NEW', 'AM', 'NEXT', 'WELL', 'PUMP', 'EVER',
+          'RUN', 'VERY', 'PLAY', 'DD', 'POST', 'ELSE', 'LOVE', 'TELL', 'BEST', 'LIFE',
+          'HOPE', 'TWO', 'NICE', 'BIT', 'MAN', 'TRUE', 'FUN', 'LOW', 'TECH', 'CAR', 'STAY',
+          'EOD', 'JOB', 'FLAT', 'OLD', 'RTX', 'HOME', 'OW', 'JOE', 'BEAT', 'WOW', 'X', 'ATH',
+          'SAVE', 'EAT', 'HUGE', 'PER', 'LIVE', 'CARE', 'PEAK', 'TURN', 'PLUS', 'HEAR', 'GAIN', 'BRO', 'RH']
 
-    main_list = [
-        item for item in masterSET if item in list_of_tickers if item not in r_item]
+main_list = [
+    item for item in masterSET if item in list_of_tickers if item not in r_item]
 
-    Counter = Counter(main_list)
-    most_occur = Counter.most_common(12)
-    mainFrame = pd.DataFrame(most_occur)
-    mainFrame.columns = ['Ticker', 'Frequency']
+Counter = Counter(main_list)
+most_occur = Counter.most_common(12)
+mainFrame = pd.DataFrame(most_occur)
+mainFrame.columns = ['Ticker', 'Frequency']
 
-    #temp = mainFrame.plot(kind='bar', x='Ticker', y='Frequency')
+# temp = mainFrame.plot(kind='bar', x='Ticker', y='Frequency')
 
-    plt.figure(1)
-    dateformat = date.today().strftime("%B %d, %Y")
-    ax = sb.barplot(x='Ticker', y='Frequency', data=mainFrame)
-    plt.title("WSB DAILY: THE MOST POPULAR TICKERS ON R/WSB TODAY", fontsize=20)
-    annotation = ('--------- Summary ---------\n'+sentiment + '\nCalls: '+str(callCount)+'\nPuts: '+str(putCount)+'\nAutist Count: ' +
-                  str(autistCount)+'\nAnalyzed '+str(commentCounter)+' comments'+'\nToday is: '+dateformat+'\n---------------------------')
+plt.figure(1)
+dateformat = date.today().strftime("%B %d, %Y")
+ax = sb.barplot(x='Ticker', y='Frequency', data=mainFrame)
+plt.title("WSB DAILY: THE MOST POPULAR TICKERS ON R/WSB TODAY", fontsize=20)
+annotation = ('--------- Summary ---------\n'+sentiment + '\nCalls: '+str(callCount)+'\nPuts: '+str(putCount)+'\nAutist Count: ' +
+              str(autistCount)+'\nAnalyzed '+str(commentCounter)+' comments'+'\nToday is: '+dateformat+'\n---------------------------')
 
-    plt.text(0.9, 0.5, annotation,
-             horizontalalignment='center',
-             verticalalignment='top',
-             transform=ax.transAxes)
+plt.text(0.9, 0.5, annotation,
+         horizontalalignment='center',
+         verticalalignment='top',
+         transform=ax.transAxes)
 
-    plt.subplots_adjust(left=0.06, bottom=0.07, right=0.97,
-                        top=0.94, wspace=None, hspace=None)
+plt.subplots_adjust(left=0.06, bottom=0.07, right=0.97,
+                    top=0.94, wspace=None, hspace=None)
 
-    print(spacer)
+print(spacer)
 
-    end = time.time()
-    print("Run time: " + str(round(end - start, 2))+" seconds")
+end = time.time()
+print("Run time: " + str(round(end - start, 2))+" seconds")
 
-    plt.show()
-
-
-getSentiment(20, 20)
+plt.show()
