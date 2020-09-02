de4150c4f14c64980d8f296be001195fbfe1870d
Sam <sampom100@gmail.com>
2020-09-01 19:00:04 -0500

    new image

--- src/secret.py
+++ src/secret.py
@@ -3,8 +3,8 @@
 # https://www.reddit.com/prefs/apps #
 
 
-client_id = "xyz"
+client_id = "gOj5ab2W56tTpg"
 
-client_secret = "abc"
+client_secret = "5SFP6ERFN7HSZnX_plB2_HqVNu4"
 
 user_agent = "MarketTrack"
